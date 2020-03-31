ede297bd5427245023f506bfb9ac179ed2ad8056
ethaneas <hongjiang93@hotmail.com>
2020-03-31 12:51:51 -0700

    Draft submitted

--- vancouver.R
+++ vancouver.R
@@ -5,7 +5,8 @@ libraries <- c('data.table', 'dplyr', 'chron'
                , 'mapproj', 'bit64', 'ggmap'
                , 'sna', 'gridExtra', 'RColorBrewer'
                , 'ggraph', 'ggalluvial', 'alluvial'
-               , 'forcats', 'grDevices')
+               , 'forcats', 'grDevices', 'ggpubr'
+               , 'plyr')
 lapply(libraries, require, character.only = T)
 
 
@@ -27,24 +28,52 @@ spring <- lapply(1:4, FUN = function(x){
   df <- paste0('df',x) 
   file <- paste0('2018-0',x,'.csv')
   df <- fread(file)
-  df <- df[,c(1:6)]
-  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name")
+  df <- df[,c(1:9)]
+  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name"
+                 , "formula", "distance_m", "duration_s")
   df <- df[,.(start.date = as.POSIXct(start.date, format = "%Y-%m-%d %H:%M")
               , end.date = as.POSIXct(end.date, format = "%Y-%m-%d %H:%M")
               , start.station.name
-              , end.station.name)]
+              , end.station.name
+              , formula
+              , distance_m
+              , duration_s)]
+  df <- df[!(start.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                       , "1000 Temporary Events Station"
+                                       , "0997 WareHouse Workshop"
+                                       , "0999 Bike Production"
+                                       , "0996 Balancer Bike Check In"))
+           & !(end.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                       , "1000 Temporary Events Station"
+                                       , "0997 WareHouse Workshop"
+                                       , "0999 Bike Production"
+                                       , "0996 Balancer Bike Check In")),]
 })
 
 summer <- lapply(5:8, FUN = function(x){
   df <- paste0('df',x) 
   file <- paste0('2018-0',x,'.csv')
   df <- fread(file)
-  df <- df[,c(1:6)]
-  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name")
+  df <- df[,c(1:9)]
+  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name"
+                 , "formula", "distance_m", "duration_s")
   df <- df[,.(start.date = as.POSIXct(start.date, format = "%Y-%m-%d %H:%M")
               , end.date = as.POSIXct(end.date, format = "%Y-%m-%d %H:%M")
               , start.station.name
-              , end.station.name)]
+              , end.station.name
+              , formula
+              , distance_m
+              , duration_s)]
+  df <- df[!(start.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                              , "1000 Temporary Events Station"
+                                              , "0997 WareHouse Workshop"
+                                              , "0999 Bike Production"
+                                              , "0996 Balancer Bike Check In"))
+                  & !(end.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                              , "1000 Temporary Events Station"
+                                              , "0997 WareHouse Workshop"
+                                              , "0999 Bike Production"
+                                              , "0996 Balancer Bike Check In")),]
 })
 
 winter <- lapply(9:12, FUN = function(x){
@@ -55,46 +84,90 @@ winter <- lapply(9:12, FUN = function(x){
     paste0('2018-',x,'.csv')
   }
   df <- fread(file)
-  df <- df[,c(1:6)]
-  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name")
+  df <- df[,c(1:9)]
+  names(df) <- c("start.date", "end.date", "account.id", "bike.id", "start.station.name", "end.station.name"
+                 , "formula", "distance_m", "duration_s")
   df <- df[,.(start.date = as.POSIXct(start.date, format = "%Y-%m-%d %H:%M")
               , end.date = as.POSIXct(end.date, format = "%Y-%m-%d %H:%M")
               , start.station.name
-              , end.station.name)]
+              , end.station.name
+              , formula
+              , distance_m
+              , duration_s)]
+  df <- df[!(start.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                       , "1000 Temporary Events Station"
+                                       , "0997 WareHouse Workshop"
+                                       , "0999 Bike Production"
+                                       , "0996 Balancer Bike Check In"))
+           & !(end.station.name %in% c("1000 Temporary Station - Marketing Events"
+                                       , "1000 Temporary Events Station"
+                                       , "0997 WareHouse Workshop"
+                                       , "0999 Bike Production"
+                                       , "0996 Balancer Bike Check In")),]
 })
 
 df_spring <- do.call(rbind,spring) %>% na.omit() 
 dim(df_spring)
-# df_spring contains 122,095 observations and 4 variables
+# df_spring contains 121792 observations and 7 variables
 # df_spring from Jan 2018 - April 2018
 
-df_summer <- do.call(rbind,summer) %>% na.omit() 
+df_summer <- do.call(rbind,summer) %>% na.omit()
 dim(df_summer)
-# df_summer contains 401,390 observations and 4 variables
+# df_summer contains 399763 observations and 7 variables
 # df_summer from May 2018 - August 2018
 
 df_winter <- do.call(rbind,winter) %>% na.omit() 
 dim(df_winter)
-# df_winter contains 209,587 observations and 4 variables
+# df_winter contains 209273 observations and 7 variables
 # df_winter from Sept 2018 - Dec 2018
 
 # removing weekends
-df_spring <- df_spring[order(start.date)][is.weekend(start.date) == F,]
+df_spring <- df_spring[order(start.date)][!(weekdays(start.date) %in% c("Saturday", "Sunday")) &
+                                            !(weekdays(end.date) %in% c("Saturday", "Sunday")),]
 dim(df_spring)
 
-df_summer <- df_summer[order(start.date)][is.weekend(start.date) == F,]
+df_summer <- df_summer[order(start.date)][!(weekdays(start.date) %in% c("Saturday", "Sunday")) &
+                                            !(weekdays(end.date) %in% c("Saturday", "Sunday")),]
 dim(df_summer)
 
-df_winter <- df_winter[order(start.date)][is.weekend(start.date) == F,]
+df_winter <- df_winter[order(start.date)][!(weekdays(start.date) %in% c("Saturday", "Sunday")) &
+                                            !(weekdays(end.date) %in% c("Saturday", "Sunday")),]
 dim(df_winter)
 
-write.csv(df, "df_spring.csv")
-write.csv(df, "df_summer.csv")
-write.csv(df, "df_winter.csv")
+write.csv(df_spring, "df_spring.csv")
+write.csv(df_summer, "df_summer.csv")
+write.csv(df_winter, "df_winter.csv")
+
+
+# Time series graph -----------------------------------------------------
+df <- df_summer
+temp <- df[,.(.N), by = .(weekdays(start.date), hour(start.date))][,.(Weekday = factor(weekdays, levels=c("Monday","Tuesday","Wednesday", "Thursday", "Friday"))
+                                                                    , Hour = hour
+                                                                    , Trips = N)]
+p <- ggplot(data = temp, aes(x = Hour, y = Trips)) +
+  geom_line() +
+  facet_grid(. ~ Weekday) + 
+  theme_bw() 
+p 
+
+temp2 <- df[distance_m < quantile(df[,distance_m], 0.995),]
+p2 <- ggplot(data = temp2, aes(x=distance_m/1000)) + geom_histogram() +
+  xlab("Distance travelled (km)") +
+  ylab("Trips") +
+  theme_bw()
+p2
+
+temp3 <- df[duration_s < quantile(df[,duration_s], 0.99),]
+p3 <- ggplot(data = temp3, aes(x=duration_s/60)) + geom_histogram() +
+  xlab("Duration of trip (min)") +
+  ylab("Trips") +
+  theme_bw()
+p3
+
+ggarrange(p2, p3, ncol = 2, nrow = 1)
 
 
 # Community detection -----------------------------------------------------
-
 ### read
 summer.membership <- fread("summer_membership.csv")
 setDT(summer.membership)
@@ -150,7 +223,6 @@ spring.membership <- overall_membership(df_spring)
 write.csv(summer.membership, "summer_membership.csv")
 write.csv(winter.membership, "winter_membership.csv")
 write.csv(spring.membership, "spring_membership.csv")
-
 
 ## dynamic
 summer.membership.hourly <- lapply(0:23, df = df_summer, FUN = function(h, df){
@@ -284,7 +356,7 @@ stations <- function(df){
   stations
 }
 
-stations <- stations(df)
+stations <- stations(df_summer)
 output <- lapply(stations, FUN = function(stations){
   cbind(stations, geocode(stations, output = "latlona", source = "google"))
 })
@@ -296,12 +368,12 @@ write.csv(map.dataset,"map.coordinates.csv")
 
 
 # cluster objects: fast_greedy, walks, infomap, louvain
-summer.membership <- summer.membership[,c(-1)]
-spring.membership <- spring.membership[,c(-1)]
-winter.membership <- winter.membership[,c(-1)]
 summer.membership
+setDT(summer.membership)
 spring.membership
+setDT(spring.membership)
 winter.membership
+setDT(winter.membership)
 
 summer.membership.hourly
 spring.membership.hourly
@@ -309,45 +381,92 @@ winter.membership.hourly
 
 ## overall visualization
 head(map.dataset)
-vancouver.map <- get_googlemap(center=c(lon=-123.115493,lat=49.284025),scale=2,size=c(350,350),zoom=12,maptype="roadmap")
-plot <- list()
-overall.plot <- function(df, map.data, vancouver.map){
-  df <- df[,.(stations = paste0(df[,id],', Vancouver, British Columbia'), fast_greedy, walks, infomap, louvain)]
-  cluster.set <- merge(df, map.data, by = 'stations') %>% na.omit()
-  vancouver.station <- ggmap(vancouver.map) + 
-    geom_point(data=map.data, alpha=0.8, aes(lon,lat), color="red") +
-    coord_map("mercator")
-  infomap <- ggmap(vancouver.map) +
-    geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,infomap])
-  louvain <- ggmap(vancouver.map) +
-    geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,louvain])
-  greedy <- ggmap(vancouver.map) +
-    geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,fast_greedy])
-  walks <- ggmap(vancouver.map) +
-    geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,walks])
-  plot[[1]] <- vancouver.station
-  plot[[2]] <- infomap
-  plot[[3]] <- louvain
-  plot[[4]] <- greedy
-  plot[[5]] <- walks
-  return(plot)
-}
+vancouver.map <- get_googlemap(center=c(lon=-123.115493,lat=49.284025),scale=2,size=c(350,350),zoom=12,maptype="roadmap", color = "bw")
+summer.membership <- summer.membership[,.(stations = paste0(summer.membership[,id],', Vancouver, British Columbia'), fast_greedy, walks, infomap, louvain)]
+cluster.set <- merge(summer.membership, map.dataset, by = 'stations') %>% na.omit()
 
-summer.plots <- overall.plot(summer.membership, map.dataset, vancouver.map)
-spring.plots <- overall.plot(spring.membership, map.dataset, vancouver.map)
-winter.plots <- overall.plot(winter.membership, map.dataset, vancouver.map)
+vancouver.station <- ggmap(vancouver.map) + 
+  geom_point(data=map.dataset, alpha=1.5, aes(lon,lat), color="blue", size=1) +
+  coord_map("mercator") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_bw()
+vancouver.station
 
-## 3 
+louvain <- ggmap(vancouver.map) +
+  geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,louvain], size=1) +
+  coord_map("mercator") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_bw()
+louvain
+
+walks <- ggmap(vancouver.map) +
+  geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,walks], size=1) +
+  coord_map("mercator") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  theme_bw()
+walks
+
+# interaction table
+df_summer <- df_summer[,.(start.date
+                          , end.date
+                          , start.station.name = paste0(df_summer[,start.station.name],', Vancouver, British Columbia')
+                          , end.station.name = paste0(df_summer[,end.station.name],', Vancouver, British Columbia')
+                          , formula
+                          , distance_m
+                          , duration_s)]
+louvain.summer.membership <- summer.membership[,.(stations, louvain)]
+df_summer_louvain <- merge(df_summer, louvain.summer.membership, by.x = "start.station.name", by.y = "stations", all.x = TRUE, all.y = FALSE)
+df_summer_louvain <- df_summer_louvain[,.(start.station.name
+                                          , end.station.name
+                                          , start.date
+                                          , end.date
+                                          , formula
+                                          , distance_m
+                                          , duration_s
+                                          , start.louvain = louvain)]
+df_summer_louvain2  <- merge(df_summer_louvain,louvain.summer.membership, by.x = "end.station.name", by.y = "stations", all.x = TRUE, all.y = FALSE)
+temp <- df_summer_louvain2[end.station.name == ', Vancouver, British Columbia',][, .N, .(start.louvain, end.louvain = louvain)]
+temp <- temp[order(start.louvain)]
+temp[,.(total = sum(N))]
+temp[start.louvain == end.louvain,]
+temp[end.louvain == 1,]
+louvain.summer.membership[,.N, louvain]
+View(louvain.summer.membership)
+
+# plot <- list()
+# overall.plot <- function(df, map.data, vancouver.map){
+#   df <- df[,.(stations = paste0(df[,id],', Vancouver, British Columbia'), fast_greedy, walks, infomap, louvain)]
+#   cluster.set <- merge(df, map.data, by = 'stations') %>% na.omit()
+#   vancouver.station <- ggmap(vancouver.map) + 
+#     geom_point(data=map.data, alpha=0.8, aes(lon,lat), color="red") +
+#     coord_map("mercator")
+#   infomap <- ggmap(vancouver.map) +
+#     geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,infomap])
+#   louvain <- ggmap(vancouver.map) +
+#     geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,louvain])
+#   greedy <- ggmap(vancouver.map) +
+#     geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,fast_greedy])
+#   walks <- ggmap(vancouver.map) +
+#     geom_point(data=cluster.set, aes(lon, lat), color=cluster.set[,walks])
+#   plot[[1]] <- vancouver.station
+#   plot[[2]] <- infomap
+#   plot[[3]] <- louvain
+#   plot[[4]] <- greedy
+#   plot[[5]] <- walks
+#   return(plot)
+# }
+
+# summer.plots <- overall.plot(summer.membership, map.dataset, vancouver.map)
+# spring.plots <- overall.plot(spring.membership, map.dataset, vancouver.map)
+# winter.plots <- overall.plot(winter.membership, map.dataset, vancouver.map)
+
+## dynamic visualization
 summer.membership.hourly[[1]]
 spring.membership.hourly
 winter.membership.hourly
-
-plot.hourly <- function(df){
-  
-  for(i in 1:24){
-    
-  }
-}
 
 head(results)
 temp <- melt(results, id.vars = "ID") %>% na.omit()
